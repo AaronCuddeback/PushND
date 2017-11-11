8f87cbe8e59018cd4983c07842b4a144f82c7f6e
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2017-11-11 12:15:38 -0800

    add audio buttons

--- index.php
+++ index.php
@@ -32,7 +32,36 @@
         </div>
 
         <div id="nowPlayingCenter">
+          <div class="content playerControls">
 
+            <div class="buttons">
+                <button type="button" name="shuffleButton" class="controlButton shuffle" title="Shuffle Button">
+                  <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                </button>
+
+                <button type="button" name="previousButton" class="controlButton previous" title="Previous Button">
+                  <img src="assets/images/icons/previous.png" alt="Previous">
+                </button>
+
+                <button type="button" name="playButton" class="controlButton play" title="Play Button">
+                  <img src="assets/images/icons/play.png" alt="Play">
+                </button>
+
+                <button type="button" name="pauseButton" class="controlButton pause" title="Pause Button" style="display: none;">
+                  <img src="assets/images/icons/pause.png" alt="Pause">
+                </button>
+
+                <button type="button" name="nextButton" class="controlButton next" title="Next Button">
+                  <img src="assets/images/icons/next.png" alt="Next">
+                </button>
+
+                <button type="button" name="repeatButton" class="controlButton repeat" title="Repeat Button">
+                  <img src="assets/images/icons/repeat.png" alt="Repeat">
+                </button>
+
+            </div>
+
+          </div>
         </div>
 
         <div id="nowPlayingRight">
