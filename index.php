6084650a1a4f78bc4a3338505c3352dd1f371ee0
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2017-11-11 19:36:43 -0800

    Volume Bar

--- index.php
+++ index.php
@@ -94,7 +94,18 @@
         </div>
 
         <div id="nowPlayingRight">
+          <div class="volumeBar">
 
+              <button type="button" name="volumeButton" class="controlButton volume" title="Volume Button">
+                <img src="assets/images/icons/volume.png" alt="Volume">
+              </button>
+
+            <div class="progressBar">
+              <div class="progressBarBg">
+                <div class="progress"></div>
+              </div>
+
+          </div>
         </div>
       </div>
     </div>
