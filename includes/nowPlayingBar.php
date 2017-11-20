ef6b98d1f7426f9942be631716227bf9d5154907
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2017-11-20 15:53:40 -0800

    link previous song button

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -67,6 +67,16 @@ $jsonArray = json_encode($resultArray);
       audioElement.setTime(seconds);
     }
 
+    function prevSong() {
+        if(audioElement.audio.currentTime >= 3 || currentIndex == 0) {
+            audioElement.setTime(0);
+        }
+        else {
+            currentIndex = currentIndex - 1;
+            setTrack(currentPlaylist[currentIndex], currentPlaylist, true);
+        }
+    }
+
     function nextSong() {
 
       if (repeat == true) {
@@ -166,7 +176,7 @@ $jsonArray = json_encode($resultArray);
               <img src="assets/images/icons/shuffle.png" alt="Shuffle">
             </button>
 
-            <button type="button" name="previousButton" class="controlButton previous" title="Previous Button">
+            <button type="button" name="previousButton" class="controlButton previous" title="Previous Button" onclick="prevSong()">
               <img src="assets/images/icons/previous.png" alt="Previous">
             </button>
 
