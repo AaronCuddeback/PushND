ec45a8a80ae6db2bda7c00707c22200e096baf14
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2017-11-26 21:08:54 -0800

    add artist info page

---
+++ artist.php
@@ -0,0 +1,22 @@
+<?php
+require "includes/includedFiles.php";
+
+if(isset($_GET['id'])) {
+    $artistID = $_GET['id'];
+} else {
+    header("Location: index.php");
+}
+
+$artist = new Artist($con, $artistID);
+?>
+
+<div class="entityInfo">
+    <div class="centerSection">
+        <div class="artistInfo">
+            <h1 class="artistName"><?php echo $artist->getName(); ?></h1>
+                <div class="headerButtons">
+                    <button class="button" type="button" name="button">Play</button>
+                </div>
+        </div>
+    </div>
+</div>
