2dd2f8eb733d00737118655eb7adb2795f09761f
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2017-11-28 21:07:30 -0800

    Add Albums and Songs to Artist pages

--- artist.php
+++ artist.php
@@ -10,7 +10,7 @@ if(isset($_GET['id'])) {
 $artist = new Artist($con, $artistID);
 ?>
 
-<div class="entityInfo">
+<div class="entityInfo borderBottom">
     <div class="centerSection">
         <div class="artistInfo">
             <h1 class="artistName"><?php echo $artist->getName(); ?></h1>
@@ -19,4 +19,74 @@ $artist = new Artist($con, $artistID);
                 </div>
         </div>
     </div>
+</div>
+
+<div class="trackListContainer borderBottom">
+<h2>SONGS</h2>
+    <ul class="trackList">
+        <?php
+        $songIdArray = $artist->getSongIds();
+
+        $i = 1;
+        foreach($songIdArray as $songId) {
+
+            if($i > 5){
+                break;
+            }
+
+            $albumSong = new Song($con, $songId);
+            $albumArtist = $albumSong->getArtist();
+
+            echo "<li class='trackListRow'>
+                  <div class='trackCount'>
+                    <img class='play' src='assets/images/icons/play-white.png' / onclick='setTrack(\"" . $albumSong->getID() ."\", tempPlaylist, true)'>
+                    <span class='trackNumber'>$i</span>
+                  </div>
+
+                  <div class='trackInfo'>
+                    <span class='trackName'>". $albumSong->getTitle() . "</span>
+                    <span class='artistName'>". $albumArtist->getName() . "</span>
+                  </div>
+
+                  <div class='trackOptions'>
+                    <img class='optionsButton' src='assets/images/icons/more.png'/>
+                  </div>
+
+                  <div class='trackDuration'>
+                    <span class='duration'>" . $albumSong->getDuration() . "</span>
+                  </div>
+
+                  </li>";
+
+                  $i++;
+        }
+        ?>
+
+        <script>
+          var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+          tempPlaylist = JSON.parse(tempSongIds);
+        </script>
+
+    </ul>
+  </div>
+
+  <div class="gridViewContainer">
+        <h2>ALBUMS</h2>
+    <?php
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE artist='$artistID'");
+    while($row = mysqli_fetch_array($albumQuery)) {
+
+
+        echo "<div class='gridViewItem'>
+              <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" .$row['id'] . "\")'>
+                <img src='" . $row['artworkPath'] ."' />
+
+                <div class='gridViewInfo'>"
+                  . $row['title'] .
+                "</div>
+              </span>
+            </div>";
+
+    }
+    ?>
 </div>
