43d7f386f788d95c05d7359d6e1e68987c80a0ec
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2017-12-02 15:57:12 -0800

    add search for albums functionality

--- search.php
+++ search.php
@@ -15,8 +15,7 @@ if(isset($_GET['term'])) {
 </div>
 
 <script>
-
-
+$(".searchInput").focus();
     $(function(){
         var timer;
 
@@ -29,10 +28,13 @@ if(isset($_GET['term'])) {
             }, 1500);
               
         });
-         $(".searchInput").focus(); 
     });
 
 </script>
+
+<?php 
+    if($term == "") exit();
+?>
 
 <div class="trackListContainer borderBottom">
 <h2>SONGS</h2>
@@ -118,3 +120,28 @@ if(isset($_GET['term'])) {
 
         ?>
   </div>
+
+   <div class="gridViewContainer">
+        <h2>ALBUMS</h2>
+    <?php
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE title LIKE '$term%' LIMIT 10");
+
+    if(mysqli_num_rows($albumQuery) == 0){
+            echo "<span class='noResults'>No albums found matching " . $term . "</span>";
+        }
+
+    while($row = mysqli_fetch_array($albumQuery)) {
+
+        echo "<div class='gridViewItem'>
+              <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" .$row['id'] . "\")'>
+                <img src='" . $row['artworkPath'] ."' />
+
+                <div class='gridViewInfo'>"
+                  . $row['title'] .
+                "</div>
+              </span>
+            </div>";
+
+    }
+    ?>
+</div>
