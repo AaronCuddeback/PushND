f35544e31f9576533be00e02e386a8b20c315a33
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2017-12-16 21:06:27 -0800

    update email ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,8 @@ function updateEmail(emailClass) {
   var emailValue = $("." + emailClass).val();
 
   $.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn }).done(function(response) {
-    $("." + emailClass).nextUntil(".message").text(response);
-  });
+    $("." + emailClass).nextAll(".message").text(response);
+  })
 
 }
 
